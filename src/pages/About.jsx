3992d23d61ef6a5d4420cac3b9feceddd9132794
AntoineBap <129936013+AntoineBap@users.noreply.github.com>
2025-10-27 13:38:01 +0100

    Rebuild site

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="about-container">
       <div className="about-infos">
-        <div className="about-title">À propos de</div>
+        <div className="about-title">À propos de moi</div>
         <div className="about">Je m'appelle Antoine Baptista Martini, développeur web en région parisienne. 
           Après une réorientation en provenance des mathématiques, j'ai découvert le développement web et j’ai décidé de m'y consacrer pleinement. Une passion s'est éveillée en moi, séduit par l'aspect visuel, créatif du monde du web. Ma réorientation m'a permis d'équilibrer mon travail entre rigueur et créativité. 
           J'ai rapidement acquis des connaissances poussées en <strong>front-end</strong> et <strong>back-end</strong>, me démarquant par <strong>ma vitesse d’apprentissage</strong>, ainsi que ma <strong>motivation</strong> et ma <strong>rigueur</strong>.   
